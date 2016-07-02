e1ddfe518e761e5c38f2bcc2a459ed6b5d4dd232
Peters <Tosypitarz@gmail.com>
2016-07-02 01:38:38 -0500

    Bug fixes
    Error in viewController.Swift fixed

---
+++ ViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  TrueFalseStarter
+//
+//  Created by Pasan Premaratne on 3/9/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+import GameKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    var indexOfSelectedQuestion: Int = 0
+    
+    var gameSound: SystemSoundID = 0
+    
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadGameStartSound()
+        // Start game
+        playGameStartSound()
+        displayQuestion()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func displayQuestion() {
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(trivia.count)
+        let questionDictionary = trivia[indexOfSelectedQuestion]
+        questionField.text = questionDictionary.question
+
+        playAgainButton.hidden = true
+    }
+    
+    func displayScore() {
+        // Hide the answer buttons
+        button1.hidden = true
+        button2.hidden = true
+        button3.hidden = true
+        button4.hidden = true
+        
+        // Display play again button
+        playAgainButton.hidden = false
+        
+        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+        
+    }
+    
+    @IBAction func checkAnswer(sender: UIButton) {
+        // Increment the questions asked counter
+        questionsAsked += 1
+        
+        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
+        var correctAnswer = false
+        
+        if selectedQuestionDict.answer1.correct == true {
+            correctAnswer = selectedQuestionDict.answer1.correct
+        } else if selectedQuestionDict.answer2.correct == true {
+            correctAnswer = selectedQuestionDict.answer2.correct
+        } else if selectedQuestionDict.answer3.correct == true {
+            correctAnswer = selectedQuestionDict.answer3.correct
+        } else if selectedQuestionDict.answer4.correct == true {
+            correctAnswer = selectedQuestionDict.answer4.correct
+        }
+        
+        if (sender === button1 && correctAnswer == true) || (sender === button2 && correctAnswer == true) || (sender === button1 && correctAnswer == true) || (sender === button2 && correctAnswer == true)
+        {
+            correctQuestions += 1
+            questionField.text = "Correct!"
+        } else {
+            self.questionField.text = "Sorry, wrong answer!"
+        }
+        
+        trivia.removeAtIndex(indexOfSelectedQuestion)
+        
+        loadNextRoundWithDelay(seconds: 2)
+    }
+    
+    func nextRound() {
+        if questionsAsked == questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // Continue game
+            displayQuestion()
+        }
+    }
+    
+    @IBAction func playAgain() {
+        // Show the answer buttons
+        button1.hidden = false
+        button2.hidden = false
+        button3.hidden = false
+        button4.hidden = false
+        
+        
+        questionsAsked = 0
+        correctQuestions = 0
+        nextRound()
+    }
+    
+    
+    // MARK: Helper Methods
+    
+    func loadNextRoundWithDelay(seconds seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, delay)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
+            self.nextRound()
+        }
+    }
+    
+    func loadGameStartSound() {
+        let pathToSoundFile = NSBundle.mainBundle().pathForResource("GameSound", ofType: "wav")
+        let soundURL = NSURL(fileURLWithPath: pathToSoundFile!)
+        AudioServicesCreateSystemSoundID(soundURL, &gameSound)
+    }
+    
+    func playGameStartSound() {
+        AudioServicesPlaySystemSound(gameSound)
+    }
+}
+
